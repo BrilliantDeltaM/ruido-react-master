2db9153d2718eb5bf4012cc77406de9c0ada701f
BrilliantDeltaM <misaelcorrales17@gmail.com>
2025-01-15 20:04:10 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://BrilliantDeltaM.gitlab.io/react-ruido",
+  base: "https://brilliantdeltam.github.io/react-ruido/",
 })
