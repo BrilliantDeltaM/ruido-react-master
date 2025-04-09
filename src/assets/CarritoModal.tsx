1a4f010107b6d56114f8c7ba175ed26852bb8f61
BrilliantDeltaM <mispadresmeabandonaron@hotmail.com>
2025-04-09 00:44:26 -0300

    plsss

--- src/assets/CarritoModal.tsx
+++ src/assets/CarritoModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useCarrito } from "../context/CarritoContext.tsx";
-import OptionsHolder from "./menuOpciones.jsx";
+import OptionsHolder from "./MenuOpciones.jsx";
 
 interface CarritoModalProps {
     isOpen: boolean;
